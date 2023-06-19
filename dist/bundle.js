a50f6412666ea235e01e6a22d7abd855f08d4005
Nathan Krasney <natankrasney@gmail.com>
2023-06-19 10:31:52 +0300

    youtube version 18-jun2-2023

--- dist/bundle.js
+++ dist/bundle.js
@@ -4,6 +4,6 @@ function sum(n1,n2){
     return n1+n2;
 }
 
-console.log(`sum(11,2) : ${sum(11, 2)}`);
+console.log(`sum(11,2) : ${sum(11, 4)}`);
 
 console.log(`tomorrow : ${dayjs().add(1,'d')}`);
--- src/main-with-dynamic-import.js
+++ src/main-with-dynamic-import.js
@@ -2,4 +2,4 @@ import { sum } from "./utils-a.js";
 const { mul } = await import("./utils-b.js");
 
 console.log(`sum(11,2) : ${sum(11, 2)}`);
-console.log(`mul(11,2) : ${mul(11, 2)}`);
+console.log(`mul(11,2) : ${mul(11, 4)}`);
